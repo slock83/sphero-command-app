a502d1b8014cb28c7b5756a2dfee8bcc480a96a5
baldor_ <darkfrancky74@gmail.com>
2015-05-25 22:49:15 +0200

    worldViewer v0.1 (can render a map, border is missing, sphero too)

--- src/MapDiscovery/MapViewer.cpp
+++ src/MapDiscovery/MapViewer.cpp
@@ -1,14 +1,53 @@
 #include "MapViewer.h"
 #include "ui_MapViewer.h"
 
-MapViewer::MapViewer(QWidget *parent) :
-	QWidget(parent),
+#include <QPainter>
+
+MapViewer::MapViewer(WorldMap *map, QWidget *parent) :
+	QWidget(parent), _map(map),
 	ui(new Ui::MapViewer)
 {
 	ui->setupUi(this);
+	QTimer *timer = new QTimer(this);
+	connect(timer, SIGNAL(timeout()), this, SLOT(update()));
+	timer->start(1000);
 }
 
 MapViewer::~MapViewer()
 {
 	delete ui;
+}
+
+void MapViewer::paintEvent(QPaintEvent *event)
+{
+	int xmin(0), ymin(0), xmax(0), ymax(0);
+	for(pair<coord_t, point_struct_t*> c : _map->getMap())
+	{
+		coord_t coord = c.first;
+
+		if(coord.x < xmin)
+			xmin = coord.x;
+		else if(coord.x > xmax)
+			xmax = coord.x;
+
+
+		if(coord.y < ymin)
+			ymin = coord.y;
+		else if(coord.y > ymax)
+			ymax = coord.y;
+	}
+
+	QPainter painter(this);
+	painter.translate(-xmin, -ymin);
+	painter.scale(width()/(1.0*(xmax-xmin)), height()/(1.0*(ymax-ymin)));
+
+	QColor c(0, 0, 0);
+
+	painter.setBrush(c);
+
+	for(pair<coord_t, point_struct_t*> c : _map->getMap())
+	{
+		coord_t coord = c.first;
+		painter.drawPoint(coord.x, coord.y);
+	}
 }
--- src/MapDiscovery/MapViewer.h
+++ src/MapDiscovery/MapViewer.h
@@ -3,6 +3,8 @@
 
 #include <QWidget>
 
+#include "WorldMap.hpp"
+
 namespace Ui {
 	class MapViewer;
 }
@@ -12,11 +14,15 @@ class MapViewer : public QWidget
 		Q_OBJECT
 
 	public:
-		explicit MapViewer(QWidget *parent = 0);
+		explicit MapViewer(WorldMap* map, QWidget *parent = 0);
 		~MapViewer();
 
 	private:
 		Ui::MapViewer *ui;
+		WorldMap* _map;
+
+
+		void paintEvent(QPaintEvent *event);
 };
 
 #endif // MAPVIEWER_H
